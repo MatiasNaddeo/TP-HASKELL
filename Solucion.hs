fd9b7e64a2facef52f4523cd4fcaaebb69b5e0cc
MatiasNaddeo <143460440+MatiasNaddeo@users.noreply.github.com>
2024-05-20 11:29:01 -0300

    Update Solucion.hs

--- Solucion.hs
+++ Solucion.hs
@@ -54,7 +54,13 @@ frecuencia _ = [16.666668,0.0,0.0,0.0,16.666668,0.0,0.0,0.0,0.0,0.0,0.0,33.33333
 -- Ej 8
 cifradoMasFrecuente :: String -> Int -> (Char, Float)
 cifradoMasFrecuente _ _ = ('o', 33.333336)
-
+cifradoMasFrecuente :: String -> Int -> (Char, Float)
+cifradoMasFrecuente (x:xs) y | (frecuencia (x:xs))[((ord x)-97)] == maximo (frecuencia (x:xs)) = (x, maximo(frecuencia (x:xs)))
+                             | otherwise = cifradoMasFrecuente (xs++[x]) y
+-- el primer int siempre tiene que ser 0 para que arranque a contar desde 0
+obtenerEnesimoELemento:: [N] -> Int-> Int-> N
+obtenerEnesimoELemento (x:xs) z n | z==n = x
+                                  | otherwise = obtenerEnesimoELemento xs (z+1) n
 -- EJ 9
 esDescifrado :: String -> String -> Bool
 esDescifrado _ _ = False
